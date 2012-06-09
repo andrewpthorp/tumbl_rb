52c8dc5565373ec1b4a7e90384bfce013d0eaead
Andrew Thorp <andrewpthorp@gmail.com>
2012-06-09 01:19:24 -0400

    tests (and stubbed test) for TUmblRb

--- spec/tumbl_rb_spec.rb
+++ spec/tumbl_rb_spec.rb
@@ -8,4 +8,18 @@ describe TumblRb do
     end
   end
 
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      TumblRb.respond_to?(:new, true).should be_true
+    end
+
+    it "should be false if the method does not exist" do
+      TumblRb.respond_to?(:rollins, true).should be_false
+    end
+  end
+
+  describe ".delegate" do
+    it "should delegate missing methods to TumblRb::Client"
+  end
+
 end
