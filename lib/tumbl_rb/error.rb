bdb490746d99bb96eee2dcad1c0e0ba103012abc
Andrew Thorp <andrewpthorp@gmail.com>
2012-06-10 23:10:05 -0400

    added HTTP 401 to error.rb

--- lib/tumbl_rb/error.rb
+++ lib/tumbl_rb/error.rb
@@ -1,6 +1,10 @@
 module TumblRb
+
   # Custom error class for rescuing from all Tumblr errors
   class Error < StandardError; end
+
+  # 401 HTTP Status Code
+  class Unauthorized < Error; end
 
   # 404 HTTP Status Code
   class NotFound < Error; end
