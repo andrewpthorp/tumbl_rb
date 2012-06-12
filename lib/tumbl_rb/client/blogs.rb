1f22cf06aa2f9e054d3213100475d0307a8f7b1c
Andrew Thorp <andrewpthorp@gmail.com>
2012-06-11 22:51:39 -0400

    added avatar method for blogs

--- lib/tumbl_rb/client/blogs.rb
+++ lib/tumbl_rb/client/blogs.rb
@@ -10,7 +10,19 @@ module TumblRb
       # @example
       #   TumblRb.info("andrewpthorp")
       def info(blog)
-        get("/v2/blog/#{Blog.new blog}/info", {}, false)
+        get("/v2/blog/#{Blog.new blog}/info", {}, false, false)
+      end
+
+      # Get the avatar of a blog
+      #
+      # @param [String, Hash] blog to get avatar from
+      # @param [Integer] size of integer (16, 24, 30, 40, 48, 64, 96, 128, 512)
+      #
+      # @return [Hashie::Mash]
+      # @example
+      #   TumblRb.avatar("andrewpthorp", 512)
+      def avatar(blog, size)
+        get("/v2/blog/#{Blog.new blog}/avatar/#{size}", false, false)
       end
 
     end
